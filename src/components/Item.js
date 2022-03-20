1196ecd890804ea7624acdede0f8c7766e032a3b
brianko <kobj11@gmail.com>
2022-03-19 22:46:51 -0400

    inprogress

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ inCart, setInCart ] = useState(true)
+  const [ cartState, setCartState] = useState(true)
+  const cartStatus = inCart ? "" : "in-cart"
+  const btnStatus = cartState ? "Add to Cart" : "Remove from Cart"
+  const cartClick = ()=> inCart ? false : true
+  const btnClick = ()=> cartState ? false : true
+
+  console.log(cartStatus)
   return (
-    <li className="">
+    <li className={cartStatus}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={()=>{setInCart(cartClick()); setCartState(btnClick())}}>{btnStatus}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+ const [ selectedCategory, setSelectedCategory ] = useState("All")
+  
+  console.log(selectedCategory)
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
